d362b38e30a9206afd90e38fb3b3fd135d01dc42
a0970303505-dot <a0970303505@gmail.com>
2026-05-22 12:56:07 +0800

    fastapi

--- src/calculate.py
+++ src/calculate.py
@@ -1,10 +1,19 @@
-def add_func(a, b):
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+def add_func(a: float, b: float) -> float:
     return a + b
 
 
-def sub_func(a, b):
-    return a - b
+@app.get("/")
+def home() -> dict[str, str]:
+    return {"status": "Online", "message": "這是簡易計算機 API"}
 
 
-def mut_func(a: float, b: float) -> float:
-    return a * b
+@app.get("/add")
+def calculate_add(a: float, b: float) -> dict[str, any]:
+    # # 使用上面定義的 add_func 進行計算
+    result: float = add_func(a, b)
+    return {"operation": "addition", "a": a, "b": b, "result": result}
